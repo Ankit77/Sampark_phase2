776d4352028335d6ac4852fda560bcd7f64fab1f
Ankit Khatri <ankit.khatri@indianic.com>
2017-05-26 17:53:49 +0530

    Implement CheckIn/Checkout Success form app side

--- app/src/main/java/com/symphony/http/WSGetMeter.java
+++ app/src/main/java/com/symphony/http/WSGetMeter.java
@@ -3,6 +3,7 @@ package com.symphony.http;
 import android.content.Context;
 
 import com.symphony.E_Sampark;
+import com.symphony.utils.Const;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -70,7 +71,7 @@ public class WSGetMeter {
                         break;
                     case XmlPullParser.END_TAG:
                         if (tagname.equalsIgnoreCase("Meter")) {
-                            //e_sampark.getSharedPreferences().edit().putInt(Const.PREF_CHECKIN_METER, Integer.parseInt(text));
+                            e_sampark.getSharedPreferences().edit().putInt(Const.PREF_CHECKIN_METER, Integer.parseInt(text));
                         }
                         break;
                     default:
